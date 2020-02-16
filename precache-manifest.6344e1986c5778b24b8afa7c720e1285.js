ce8dfabbf606925e70ac0ffc68e61444a6fb8fa1
Kumar Aryan <idkaryan@gmail.com>
2020-02-16 21:23:47 +0530

    react build

---
+++ precache-manifest.6344e1986c5778b24b8afa7c720e1285.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf488cf70f657d1b4c3bc54fad66f1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "661c34d6c2f5809d3b1a",
+    "url": "/static/css/main.85ec0172.chunk.css"
+  },
+  {
+    "revision": "570afe931244a06b2dc2",
+    "url": "/static/js/2.0392d7f7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0392d7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661c34d6c2f5809d3b1a",
+    "url": "/static/js/main.4553d24d.chunk.js"
+  },
+  {
+    "revision": "cd6a5f832bb12a4158e3",
+    "url": "/static/js/runtime-main.2aef1696.js"
+  },
+  {
+    "revision": "ec6ccff33239a4ee42a0a013db3b0a47",
+    "url": "/static/media/aryan.ec6ccff3.png"
+  },
+  {
+    "revision": "08555e63d92cafd384ef05134b7307b6",
+    "url": "/static/media/csharp.08555e63.png"
+  },
+  {
+    "revision": "a470baa362b39be8f8d3f942cb2b1f14",
+    "url": "/static/media/godot.a470baa3.svg"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/static/media/javascript.0e4fdce8.png"
+  },
+  {
+    "revision": "2eac9857861345175222223b7a120cb8",
+    "url": "/static/media/mongodb.2eac9857.png"
+  },
+  {
+    "revision": "f825aaa7ed02836f4c9d58268116a897",
+    "url": "/static/media/mssql.f825aaa7.png"
+  },
+  {
+    "revision": "8a4ee7e2c0e7023919fc2e0164f22397",
+    "url": "/static/media/nodejs.8a4ee7e2.png"
+  },
+  {
+    "revision": "e38bad381bfe3ab54514e1745862e993",
+    "url": "/static/media/sass.e38bad38.png"
+  }
+]);
